039967bfa702d03bdddc19d4ab9bf1b4f2dab86c
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2020-12-02 17:47:49 +0100

    Add initial files for Fusion101

---
+++ Fusion101/Fusion101.py
@@ -0,0 +1,26 @@
+#Author-Julia & Sören
+#Description-Fusion101 provides you a gentle introduction into the basics of Fusion360 by providing step-by-step tutorials to multiple topics.
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Hello addin')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+def stop(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Stop addin')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
