e809aa59c2134f140a4528d45645701b7c588259
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2020-12-22 00:32:52 +0100

    Add basic vue application

---
+++ Fusion101/web/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button v-on:click="closeApp">X</button>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  methods: {
+    closeApp() {
+      var args = {
+        arg1: "Sample argument 1",
+        arg2: "Sample argument 2"
+      };
+      window.adsk.fusionSendData("send", JSON.stringify(args));
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
