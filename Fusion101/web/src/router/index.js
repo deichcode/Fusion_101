293c2fc0df5850f66c633f2292814c5f8c89d4b3
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2021-01-03 19:03:14 +0100

    Add routes for tutorials
    Added routes for the overview of a tutorial and the route for the single chapters

--- Fusion101/web/src/router/index.js
+++ Fusion101/web/src/router/index.js
@@ -1,25 +1,40 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
+// Define routes for navigation inside the palette web app accordingly to https://router.vuejs.org/guide/
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    // routing for multiple tutorials.json
+    // {
+    //   path: '/',
+    //   name: 'Overview of all available Tutorials',
+    //   component: TutorialsOverview
+    // },
+    // routing as long as only single tutorial exists
+    {
+        path: '/',
+        redirect: '/tutorial-contents/building-a-house'
+    },
+    {
+        path: '/tutorial-contents',
+        redirect: '/'
+    },
+    {
+        path: '/tutorial-contents/:tutorialId',
+        name: 'tutorial-contents',
+        component: () => import(/* webpackChunkName: "[request]" */ '../views/TutorialContents.vue') //https://vuedose.tips/naming-webpack-chunks-on-lazy-loaded-routes-in-vuejs/
+    },
+    {
+        path: '/tutorial-chapter/:tutorialId/:chapterId',
+        name: 'chapter',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "[request]" */ '../views/TutorialChapter.vue') //https://vuedose.tips/naming-webpack-chunks-on-lazy-loaded-routes-in-vuejs/
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
