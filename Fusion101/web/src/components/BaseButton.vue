66bffa55f668681b7b97c80e94f95ec03193cdb0
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2021-01-03 19:04:50 +0100

    Create a button
    Add a basic button. Since it is just a dumb component without own logic it gets the prefix 'Base' which is a convention in vue

---
+++ Fusion101/web/src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/css/variables/colors";
+button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover{
+  background-color: $highlight;
+}
+
+button:active {
+  background-color: $active;
+}
+
+</style>
