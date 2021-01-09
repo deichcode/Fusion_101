f5cf5d57998d968868f0cb84eeb8505c4d8f807e
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2021-01-09 12:37:17 +0100

    Ignore example test

--- Fusion101/web/tests/unit/example.spec.js
+++ Fusion101/web/tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import TutorialsOverview from "@/views/TutorialsOverview";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('TutorialsOverview.vue', () => {
+  xit('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(TutorialsOverview, {
       props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
