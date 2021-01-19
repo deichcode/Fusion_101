a39d17316e6f800bcc216980e8322180f4b61e9b
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2021-01-19 14:51:03 +0100

    Add font-awesome to App

--- Fusion101/web/src/main.js
+++ Fusion101/web/src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faLightbulb as regularFaLightbulb} from "@fortawesome/free-regular-svg-icons";
+import {faLightbulb} from "@fortawesome/free-solid-svg-icons";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+library.add(regularFaLightbulb)
+library.add(faLightbulb)
+library.add(faCheck)
+
 require('./css/globals/_main.scss')
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
