e602a099b55d35bbbdb2c0955136277549900299
Sören Schröder <soeren.schroeder@rwth-aachen.de>
2021-01-19 14:32:04 +0100

    Add debug switch for palette url
    If the DEBUG flag is set to false the builded version is used

--- Fusion101/constants.py
+++ Fusion101/constants.py
@@ -1,3 +1,7 @@
 PALLET_ID = 'fusion101'
 PALLET_NAME = 'Fusion 101'
-PALLET_URL = 'http://localhost:8080'
+DEBUG = True
+if DEBUG:
+    PALLET_URL = 'http://localhost:8080'
+else:
+    PALLET_URL = './web/dist/index.html'
